cc8fdb39ddc45752a7dfb8d8acd377d71e283011
GustavoLevi <1200829@isep.ipp.pt>
2022-11-04 16:09:42 +0000

    LAPR5-2, LAPR5-12, LAPR5-7, LAPR5-17

--- .net application WM/nunopsilva-dddnetcore-c2122b82f1b9/Domain/Warehouse/WarehouseDto.cs
+++ .net application WM/nunopsilva-dddnetcore-c2122b82f1b9/Domain/Warehouse/WarehouseDto.cs
@@ -9,8 +9,11 @@ namespace DDDSample1.Domain.Warehouses
 
         public string Description { get; set; }
 
-        public Address address { get; set;}
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
 
-        public Coordinates coordinates { get; set;}
+         public string Latitude { get; set; }
+        public string Longitude { get; set; }
     }
 }
