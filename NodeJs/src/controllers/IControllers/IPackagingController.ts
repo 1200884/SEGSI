83301f2b187da2e186d7325cf76f84ff53223cbb
Margarida <1200873@isep.ipp.pt>
2022-11-29 18:01:37 +0000

    LAPR5-31 mudanca de gets e de atributos para funcionar com o angular

--- NodeJs/src/controllers/IControllers/IPackagingController.ts
+++ NodeJs/src/controllers/IControllers/IPackagingController.ts
@@ -3,6 +3,7 @@ import { Request, Response, NextFunction } from 'express';
 export default interface IPackagingController  {
   createPackaging(req: Request, res: Response, next: NextFunction);
   updatePackaging(req: Request, res: Response, next: NextFunction);
-  getPackaging(req: Request, res: Response, next: NextFunction);
+  getPackagings(req: Request, res: Response, next: NextFunction);
+  getPackaging(id: string, req: Request, res: Response, next: NextFunction);
   patchPackaging(req: Request, res: Response, next: NextFunction);
 }
