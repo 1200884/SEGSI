8ff173c5a2e2d4e965d20bd819edfe09704d92e2
Simoes 1200876 <1200876@isep.ipp.pt>
2022-11-30 10:54:49 +0000

    Pipeline error fix

--- NodeJs/src/core/domain/UniqueEntityID.ts
+++ NodeJs/src/core/domain/UniqueEntityID.ts
@@ -1,9 +1,9 @@
 
-import uuid from 'uuid/v4';
+import { v4 as uuid } from 'uuid';
 import { Identifier } from './Identifier'
 
 export class UniqueEntityID extends Identifier<string | number>{
-  constructor (id?: string | number) {
+  constructor(id?: string | number) {
     super(id ? id : uuid())
   }
 }
