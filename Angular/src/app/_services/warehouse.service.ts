282a07ff6ae4fb4c2608f345bbed054277a9cfb8
Simoes 1200876 <1200876@isep.ipp.pt>
2022-12-30 14:44:32 +0000

    LAPR5-109 Adding UI and fixing previous Truck-related errors

--- Angular/src/app/_services/warehouse.service.ts
+++ Angular/src/app/_services/warehouse.service.ts
@@ -3,30 +3,31 @@ import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { Warehouse } from '../_models/Warehouse';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WarehouseService {
 
-  private apiUri = "https://localhost:5001/api";
-  private Warehouses = "/Warehouses"
+  //private apiUri = "https://localhost:5001/api";
+  //private Warehouses = "/Warehouses"
   ipAddress = '';
   constructor(private http: HttpClient) { }
 
   getWarehouses(): Observable<Warehouse[]> {
-    return this.http.get<Warehouse[]>(this.apiUri + this.Warehouses,{observe: 'body', responseType: 'json'});
+    return this.http.get<Warehouse[]>(environment.WAREHOUSE_URL_LOCAL + environment.WAREHOUSES_URL, { observe: 'body', responseType: 'json' });
   }
 
-  getWarehouse(id: string): Observable<Warehouse>{
-    return this.http.get<Warehouse>(this.apiUri + this.Warehouses + "/" + id);
+  getWarehouse(id: string): Observable<Warehouse> {
+    return this.http.get<Warehouse>(environment.WAREHOUSE_URL_LOCAL + environment.WAREHOUSES_URL + "/" + id);
   }
 
-  update(warehouse: Warehouse): Observable<any>{
-    return this.http.put(this.apiUri+this.Warehouses+ "/" + warehouse.id,warehouse);
+  update(warehouse: Warehouse): Observable<any> {
+    return this.http.put(environment.WAREHOUSE_URL_LOCAL + environment.WAREHOUSES_URL + "/" + warehouse.id, warehouse);
   }
 
-  addWarehouse(warehouse: any): Observable<Warehouse>{
-    return this.http.post<Warehouse>(this.apiUri+this.Warehouses,warehouse);
+  addWarehouse(warehouse: any): Observable<Warehouse> {
+    return this.http.post<Warehouse>(environment.WAREHOUSE_URL_LOCAL + environment.WAREHOUSES_URL, warehouse);
   }
 }
