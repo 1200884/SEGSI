4d0b27f1a1c2202f1acca56e138bd0e8a8090819
1191079 <1191079@isep.ipp.pt>
2023-01-06 01:16:59 +0000

    LAPR5-145, LAPR5-146, LAPR5-147 #comment css update

--- Angular/src/app/board-warehouse/get-deliveries/get-deliveries.component.ts
+++ Angular/src/app/board-warehouse/get-deliveries/get-deliveries.component.ts
@@ -1,7 +1,5 @@
 ﻿import { Component, OnInit } from '@angular/core';
 import { Location } from '@angular/common';
-import {Sort} from '@angular/material/sort';
-
 import { DeliveryService } from 'src/app/_services/delivery.service';
 import { Delivery } from '../../_models/Delivery';
 
@@ -24,9 +22,7 @@ export class DeliveriesComponent implements OnInit  {
   constructor(
     private deliveryService: DeliveryService,
     private location: Location,
-    
-    ) { 
-    }
+    ) { }
 
   ngOnInit(): void {
     this.getDeliveries();
