61397089d4cdbedda3885dbe053a6d5f9fb36df0
Margarida <1200873@isep.ipp.pt>
2023-01-08 22:32:20 +0000

    LAPR-147 testes unitarios em angular

--- Angular/src/app/board-logistics/put-packaging/put-packaging.component.spec.ts
+++ Angular/src/app/board-logistics/put-packaging/put-packaging.component.spec.ts
@@ -1,23 +1,43 @@
-/*import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+/*import { TestBed } from '@angular/core/testing';
 import { PutPackagingComponent } from './put-packaging.component';
+import { LogisticsService } from 'src/app/_services/logistics.service';
+import { of } from 'rxjs';
+import { Packaging } from 'src/app/_models/Packaging';
 
 describe('PutPackagingComponent', () => {
   let component: PutPackagingComponent;
-  let fixture: ComponentFixture<PutPackagingComponent>;
+  let logisticsService: LogisticsService;
+  let packaging: Packaging[];
 
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ PutPackagingComponent ]
-    })
-    .compileComponents();
+    packaging = {
+      id: '123123',
+      boxId: 0,
+      positionX: 0,
+      positionY: 0,
+      positionZ: 0
+    };
+    TestBed.configureTestingModule({
+      providers: [
+        PutPackagingComponent,
+        { provide: LogisticsService, useValue: { putPackaging: () => of({}) } },
+      ]
+    });
 
-    fixture = TestBed.createComponent(PutPackagingComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    component = TestBed.inject(PutPackagingComponent);
+    logisticsService = TestBed.inject(LogisticsService);
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should call logisticsService.putPackaging() with the correct argument when putPackaging() is called', () => {
+    const packaging: Packaging = {
+      id: 'Teste',
+      boxId: 0,
+      positionX: 0,
+      positionY: 0,
+      positionZ: 0
+    };
+    spyOn(logisticsService, 'putPackaging').and.callThrough();
+    component.putPackaging(packaging);
+    expect(logisticsService.putPackaging).toHaveBeenCalledWith(packaging);
   });
 });*/
